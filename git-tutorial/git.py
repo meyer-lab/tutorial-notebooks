287204230217fb51ee71aa449376629f65a1869a
mcreixell <46428799+mcreixell@users.noreply.github.com>
2019-04-26 12:39:57 -0700

    Marc push (#6)

--- git-tutorial/git.py
+++ git-tutorial/git.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 def hello():
+    print("Hello!")
+    print("Bye!")
+    print("one more line")
     print("Hello Peter!")
     print("Hello!")
     
 def learn git():
     x = 2
     return x
-  
+ 
